95949a805266ed1386b08343797791bc4b737983
wvdbrande <wim.vandenbrande@one-agency.be>
2017-03-30 11:13:04 +0200

    First working version of DynaRouteService

---
+++ src/main/java/be/ictdynamic/dynarouteservice/DynaRouteServiceController.java
@@ -0,0 +1,26 @@
+package be.ictdynamic.dynarouteservice;
+
+import be.ictdynamic.dynarouteservice.domain.Greeting;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class DynaRouteServiceController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DynaRouteServiceController.class);
+    private static final String TEMPLATE = "You are from %s!";
+    private static final AtomicLong COUNTER = new AtomicLong();
+
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public Greeting greeting(@RequestParam(value = "commune", defaultValue = "Edegem") String commune) {
+        return new Greeting(COUNTER.incrementAndGet(), String.format(TEMPLATE, commune));
+    }
+
+}
---
+++ src/main/java/be/ictdynamic/dynarouteservice/DynarouteserviceApplication.java
@@ -0,0 +1,11 @@
+package be.ictdynamic.dynarouteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynarouteserviceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DynarouteserviceApplication.class, args);
+	}
+}
